af78d9b6cd3443a5922052a57a8f482150983526
David Chappell <david672orford@gmail.com>
2020-05-29 16:55:58 -0400

    rejiggered templates

--- app/views.py
+++ app/views.py
@@ -1,14 +1,19 @@
 from flask import render_template
 from . import app
-from .models import db, Forums
+from .models import db, Forums, Topics
 
 @app.route("/")
 def forums_list():
-	return render_template("forums.html", title="Forums", forums=Forums.query)
+	return render_template("index.html", title="Forums", forums=Forums.query)
 
-@app.route("/<name>")
-def forums_show(name):
-	forum=Forums.query.filter_by(name=name).one()
+@app.route("/<forum_name>/")
+def forum(forum_name):
+	forum=Forums.query.filter_by(name=forum_name).one()
 	return render_template("forum.html", title=forum.name, forum=forum)
 
+@app.route("/<forum_name>/<int:topicid>")
+def forum_topic(forum_name, 
+	topic=Topics.query.filter_by(id=topicid).one()
+	assert topic.forum.name == forum_name
+	return render_template("forum_topic.html", topic=topic)
 
